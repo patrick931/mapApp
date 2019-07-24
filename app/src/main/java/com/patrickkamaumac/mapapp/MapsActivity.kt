8861a660462df3770131203d4446a7ab966db93d
Patrickkamau <patrickkamau@gmail.com>
2019-07-24 18:50:11 +0300

    Added a comment to test commit to git

--- app/src/main/java/com/patrickkamaumac/mapapp/MapsActivity.kt
+++ app/src/main/java/com/patrickkamaumac/mapapp/MapsActivity.kt
@@ -10,7 +10,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-
+// App now in git
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
